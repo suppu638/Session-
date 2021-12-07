f8fb4c51a228f59dd58d2ee8602d1a2191b3e313
suppu638 <84116266+suppu638@users.noreply.github.com>
2021-12-07 12:04:32 +0530

    pushing to remote

---
+++ NullPointerException.java
@@ -0,0 +1,11 @@
+package trainingProjects;
+
+public class NullPointerException {
+	int arr[]=null;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		NullPointerException dummy = new NullPointerException();
+		System.out.println(dummy.arr.length);
+	}
+
+}
